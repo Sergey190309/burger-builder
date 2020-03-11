c30b197cb3de664a04b556a351701d2727461970
Sergey190309 <sa6702@gmail.com>
2020-03-11 16:29:49 +0300

    Before advanced redux implementations

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import {
+  createStore
+  // applyMiddleware,
+  // compose
+} from "redux";
 
 import './index.css';
 // import App from "./App";
@@ -11,7 +15,11 @@ import App from "./App.jsx"
 import reducer from "./store/reducer";
 
 
-const store = createStore(reducer);
+const store = createStore(
+  reducer,
+  // @ts-ignore
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 const app = (
   <Provider store={store}>
